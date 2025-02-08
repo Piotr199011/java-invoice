c54dd929a4ae9a660ea16e0da80fd6f311a0f7d3
jarmosz <piotr.jarmosz@o2.pl>
2025-02-08 11:27:53 +0100

    "Commit test"

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -10,24 +10,35 @@ public abstract class Product {
     private final BigDecimal taxPercent;
 
     protected Product(String name, BigDecimal price, BigDecimal tax) {
+        if (name == null||name.isEmpty() ) {
+            throw new IllegalArgumentException("Product name cannot be null or empty!");
+        }
+
+        if (price==null||price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("Product price cannot be negative or empty!");
+        }
         this.name = name;
         this.price = price;
         this.taxPercent = tax;
     }
 
     public String getName() {
-        return null;
+
+        return name;//this.name
     }
 
     public BigDecimal getPrice() {
-        return null;
+
+        return this.price;
     }
 
     public BigDecimal getTaxPercent() {
-        return null;
+
+        return this.taxPercent;
     }
 
     public BigDecimal getPriceWithTax() {
-        return null;
+
+        return this.price.multiply(this.taxPercent).add(this.price);
     }
 }
