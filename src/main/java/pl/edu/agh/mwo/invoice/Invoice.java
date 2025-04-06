f8b025f278370a5de004d3ec0dc6802b20ed3a45
jarmosz <piotr.jarmosz@o2.pl>
2025-04-06 15:05:45 +0200

    "Commit test"

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,43 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products;
-
+    private Collection<Product> products=new ArrayList<>();
+    private HashMap<Product,Integer>productQuantity=new HashMap<>();
     public void addProduct(Product product) {
-        // TODO: implement
+      this.products.add(product);
+
     }
 
     public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
-    }
+   for (int i=0;i<quantity;i++) {
+       addProduct(product);
+   }
 
-    public BigDecimal getSubtotal() {
-        return null;
+    }
+    public BigDecimal getNetPrice() {
+
+        BigDecimal subtotal = BigDecimal.ZERO;
+        for (Product product : products) {
+            subtotal = subtotal.add(product.getPriceWithTax());
+        }
+        return subtotal;
     }
 
     public BigDecimal getTax() {
-        return null;
+
+        return BigDecimal.ZERO;
     }
 
     public BigDecimal getTotal() {
-        return null;
+
+        return BigDecimal.ZERO;
     }
 }
